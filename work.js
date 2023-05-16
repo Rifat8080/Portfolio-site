264c5084ac2786a4e97202f7e2184aa39cb7a53f
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-17 00:34:05 +0600

    Modal framework set with js css and html

---
+++ work.js
@@ -0,0 +1,51 @@
+// Function to open a modal
+function openModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'block';
+}
+
+// Function to close a modal
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'none';
+}
+
+// Add event listeners to the buttons that open the modals
+document.getElementById('popbtn1').addEventListener('click', function () {
+  openModal('myModal1');
+});
+
+document.getElementById('popbtn2').addEventListener('click', function () {
+  openModal('myModal2');
+});
+
+document.getElementById('popbtn3').addEventListener('click', function () {
+  openModal('myModal3');
+});
+
+document.getElementById('popbtn4').addEventListener('click', function () {
+  openModal('myModal4');
+});
+
+document.getElementById('popbtn5').addEventListener('click', function () {
+  openModal('myModal5');
+});
+
+document.getElementById('popbtn6').addEventListener('click', function () {
+  openModal('myModal6');
+});
+
+// Add event listeners to the close buttons in the modals
+var closeButtons = document.getElementsByClassName('close');
+for (var i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener('click', function () {
+    closeModal(this.closest('.modal1').id);
+  });
+}
+
+// Close the modal when the user clicks outside the modal content
+window.addEventListener('click', function (event) {
+  if (event.target.classList.contains('modal1')) {
+    closeModal(event.target.id);
+  }
+});
