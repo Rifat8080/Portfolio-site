9eb36e47d63ae58fb581ede0dd2cace4c75af684
rbd3 <rabedesanaa@gmail.com>
2023-05-17 13:33:54 +0300

    Add Js code for form validation

---
+++ form.js
@@ -0,0 +1,17 @@
+const validateForm = (event) => {
+    event.preventDefault(); // Prevent form submission for now
+  
+    // Perform email validation
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value.trim();
+    const lowerCaseEmail = email.toLowerCase();
+    if (email !== lowerCaseEmail) {
+      document.getElementById('error-message').textContent =
+        'Please enter a lowercase email address.';
+      return; // Stop further processing
+    }
+  
+    // Clear error message and submit the form if validation passes
+    document.getElementById('error-message').textContent = '';
+    event.target.submit();
+  };
