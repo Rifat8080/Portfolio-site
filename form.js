47786c20b7417f7c17eb9f5233936ef931e0c901
rbd3 <rabedesanaa@gmail.com>
2023-05-17 13:36:03 +0300

    Add Js code for local storage

--- form.js
+++ form.js
@@ -1,3 +1,17 @@
+// Load form data from local storage on page load
+window.addEventListener('DOMContentLoaded', () => {
+    const formData = localStorage.getItem('form_data');
+    if (formData) {
+      const { name, first_name, last_name, email, text_area } =
+        JSON.parse(formData);
+      document.getElementById('name').value = name || '';
+      document.getElementById('first_name').value = first_name || '';
+      document.getElementById('last_name').value = last_name || '';
+      document.getElementById('email').value = email || '';
+      document.getElementById('text_area').value = text_area || '';
+    }
+  });
+  
 const validateForm = (event) => {
     event.preventDefault(); // Prevent form submission for now
   
