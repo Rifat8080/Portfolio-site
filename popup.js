15ef1a5f19b5c6db258d2ac4327db2e63b59c91f
rbd3 <rabedesanaa@gmail.com>
2023-05-17 13:31:35 +0300

    Add Js and CSS file for the popup windo

---
+++ popup.js
@@ -0,0 +1,52 @@
+// Function to open a modal
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+  }
+  
+  // Function to close a modal
+  function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+  }
+  
+  // Add event listeners to the buttons that open the modals
+  document.getElementById('popbtn1').addEventListener('click', () => {
+    openModal('myModal1');
+  });
+  
+  document.getElementById('popbtn2').addEventListener('click', () => {
+    openModal('myModal2');
+  });
+  
+  document.getElementById('popbtn3').addEventListener('click', () => {
+    openModal('myModal3');
+  });
+  
+  document.getElementById('popbtn4').addEventListener('click', () => {
+    openModal('myModal4');
+  });
+  
+  document.getElementById('popbtn5').addEventListener('click', () => {
+    openModal('myModal5');
+  });
+  
+  document.getElementById('popbtn6').addEventListener('click', () => {
+    openModal('myModal6');
+  });
+  
+  // Add event listeners to the close buttons in the modals
+  
+  const closeButtons = Array.from(document.getElementsByClassName('close'));
+  closeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      closeModal(button.closest('.modal1').id);
+    });
+  });
+  
+  // Close the modal when the user clicks outside the modal content
+  window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modal1')) {
+      closeModal(event.target.id);
+    }
+  });
