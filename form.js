93d387b5b3f92413f4d98557dfebd78a4f66c32b
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 19:34:34 +0600

    indent update

--- form.js
+++ form.js
@@ -4,26 +4,26 @@ const form = document.getElementById('form');
 // Add event listener to the form submission
 
 form.addEventListener('submit', (event) => {
-event.preventDefault();
-
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const textArea = document.getElementById('text_area');
-
-localStorage.setItem('name', nameInput.value);
-localStorage.setItem('email', emailInput.value);
-localStorage.setItem('text', textArea.value);
-
-form.submit();
+  event.preventDefault();
+  
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const textArea = document.getElementById('text_area');
+  
+  localStorage.setItem('name', nameInput.value);
+  localStorage.setItem('email', emailInput.value);
+  localStorage.setItem('text', textArea.value);
+  
+  form.submit();
 });
 
 // Load stored data on page load
 window.addEventListener('load', () => {
-const storedName = localStorage.getItem('name');
-const storedEmail = localStorage.getItem('email');
-const storedText = localStorage.getItem('text');
-
-document.getElementById('name').value = storedName;
-document.getElementById('email').value = storedEmail;
-document.getElementById('text_area').value = storedText;
+  const storedName = localStorage.getItem('name');
+  const storedEmail = localStorage.getItem('email');
+  const storedText = localStorage.getItem('text');
+  
+  document.getElementById('name').value = storedName;
+  document.getElementById('email').value = storedEmail;
+  document.getElementById('text_area').value = storedText;
 });
