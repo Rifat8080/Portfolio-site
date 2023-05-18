8567cca68b29a501d6e02c8ac608ad57ef182232
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 19:11:17 +0600

    remove extra codes

--- form.js
+++ form.js
@@ -12,14 +12,3 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('text_area').value = textArea || '';
   }
 });
-// Js validation
-const smallElement = document.getElementById('error-message');
-const emailElement = document.getElementById('email');
-const formElement = document.getElementById('form_id');
-
-formElement.addEventListener('submit', (event) => {
-  if (emailElement.value.toLowerCase() !== emailElement.value) {
-    event.preventDefault();
-    smallElement.innerText = 'please type all email charaters in lowercase.';
-  }
-});
