ab56f7dadca18bc0effc1f8e89cb1200617ad743
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 13:54:02 +0600

    js form validation updated

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
-const smallElement = document.getElementById('error');
-const emailElement = document.getElementById('email_id');
+const smallElement = document.getElementById('error-message');
+const emailElement = document.getElementById('email');
 const formElement = document.getElementById('form_id');
 
 formElement.addEventListener('submit', (event) => {
   if (emailElement.value.toLowerCase() !== emailElement.value) {
     event.preventDefault();
-    smallElement.innerText = 'please type all email charaters in lowercase. form was not submitted';
+    smallElement.innerText = 'please type all email charaters in lowercase.';
   }
 });
