351959a3cc07eee88fa5c68b44cd4ccd89110b34
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 14:53:44 +0600

    est lint update

--- form.js
+++ form.js
@@ -3,16 +3,15 @@ window.addEventListener('DOMContentLoaded', () => {
   const formData = localStorage.getItem('form_data');
   if (formData) {
     const { name, firstName, lastName, email, textArea } =
-        JSON.parse(formData);
-      document.getElementById('name').value = name || '';
-      document.getElementById('first_name').value = firstName || '';
-      document.getElementById('last_name').value = lastName || '';
-      document.getElementById('email').value = email || '';
-      document.getElementById('text_area').value = textArea || '';
+      JSON.parse(formData);
+    document.getElementById('name').value = name || '';
+    document.getElementById('first_name').value = firstName || '';
+    document.getElementById('last_name').value = lastName || '';
+    document.getElementById('email').value = email || '';
+    document.getElementById('text_area').value = textArea || '';
     }
   });
   
-
 // Js validation
 const smallElement = document.getElementById('error-message');
 const emailElement = document.getElementById('email');
