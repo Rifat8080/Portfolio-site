9740859b807c45d84cc4d5f635442098a80cf326
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 14:52:08 +0600

    js camel case variable fix

--- form.js
+++ form.js
@@ -1,14 +1,14 @@
 // Load form data from local storage on page load
 window.addEventListener('DOMContentLoaded', () => {
-    const formData = localStorage.getItem('form_data');
-    if (formData) {
-      const { name, first_name, last_name, email, text_area } =
+  const formData = localStorage.getItem('form_data');
+  if (formData) {
+    const { name, firstName, lastName, email, textArea } =
         JSON.parse(formData);
       document.getElementById('name').value = name || '';
-      document.getElementById('first_name').value = first_name || '';
-      document.getElementById('last_name').value = last_name || '';
+      document.getElementById('first_name').value = firstName || '';
+      document.getElementById('last_name').value = lastName || '';
       document.getElementById('email').value = email || '';
-      document.getElementById('text_area').value = text_area || '';
+      document.getElementById('text_area').value = textArea || '';
     }
   });
   
