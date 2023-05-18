9aa81b89801dc5bcb942df6761c71fe07a022344
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 14:46:06 +0600

    updated with validate code

--- form.js
+++ form.js
@@ -13,3 +13,14 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   
 
+// Js validation
+const smallElement = document.getElementById('error-message');
+const emailElement = document.getElementById('email');
+const formElement = document.getElementById('form_id');
+
+formElement.addEventListener('submit', (event) => {
+  if (emailElement.value.toLowerCase() !== emailElement.value) {
+    event.preventDefault();
+    smallElement.innerText = 'please type all email charaters in lowercase.';
+  }
+});
