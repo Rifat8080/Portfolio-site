1f82f9b082093fccedadb0bebcf9c7b297661b72
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 15:32:18 +0600

    js update

--- form.js
+++ form.js
@@ -1,3 +1,18 @@
+// Load form data from local storage on page load
+window.addEventListener('DOMContentLoaded', () => {
+  const formData = localStorage.getItem('form_data');
+  if (formData) {
+    const {
+      name, firstName, lastName, email, textArea,
+    } = JSON.parse(formData);
+    document.getElementById('name').value = name || '';
+    document.getElementById('first_name').value = firstName || '';
+    document.getElementById('last_name').value = lastName || '';
+    document.getElementById('email').value = email || '';
+    document.getElementById('text_area').value = textArea || '';
+  }
+});
+// Js validation
 const smallElement = document.getElementById('error-message');
 const emailElement = document.getElementById('email');
 const formElement = document.getElementById('form_id');
