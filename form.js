db7435a9545a3e9edafd51c7e03c4c43ae334c90
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-05-18 19:29:35 +0600

    eslint fixed

--- form.js
+++ form.js
@@ -1,34 +1,29 @@
 // Get form element and submit button
 const form = document.getElementById('form');
-const submitButton = document.getElementById('contact-submit');
 
 // Add event listener to the form submission
-form.addEventListener('submit', function(event) {
-  // Prevent the default form submission
-  event.preventDefault();
 
-  // Get input values
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-  const textArea = document.getElementById('text_area');
+form.addEventListener('submit', (event) => {
+event.preventDefault();
 
-  // Store the form data in local storage
-  localStorage.setItem('name', nameInput.value);
-  localStorage.setItem('email', emailInput.value);
-  localStorage.setItem('text', textArea.value);
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const textArea = document.getElementById('text_area');
 
-  // Submit the form
-  form.submit();
+localStorage.setItem('name', nameInput.value);
+localStorage.setItem('email', emailInput.value);
+localStorage.setItem('text', textArea.value);
+
+form.submit();
 });
 
 // Load stored data on page load
-window.addEventListener('load', function() {
-  const storedName = localStorage.getItem('name');
-  const storedEmail = localStorage.getItem('email');
-  const storedText = localStorage.getItem('text');
+window.addEventListener('load', () => {
+const storedName = localStorage.getItem('name');
+const storedEmail = localStorage.getItem('email');
+const storedText = localStorage.getItem('text');
 
-  // Populate form fields with stored data
-  document.getElementById('name').value = storedName;
-  document.getElementById('email').value = storedEmail;
-  document.getElementById('text_area').value = storedText;
+document.getElementById('name').value = storedName;
+document.getElementById('email').value = storedEmail;
+document.getElementById('text_area').value = storedText;
 });
